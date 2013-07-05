b614e7c99eb75d095eb0ce917df0b3885bd47e00
tps <tps@tps2.(none)>
2013-07-05 11:58:36 +0530

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,16 @@ def show
 end
 
   def new
+  	@user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  
 end
